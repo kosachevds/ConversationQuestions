ce2a7d997b319fb9bc3115ad21818ddc64fdbbf1
Kosachev Daniil <kos-dan@mail.ru>
2021-09-04 19:55:52 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/Syfaro/telegram-bot-api"
+)
+
+func main() {
+	token := os.Args[1]
+	bot, err := tgbotapi.NewBotAPI(token)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	ucfg := tgbotapi.NewUpdate(0)
+
+	ucfg.Timeout = 60
+	updatesChan, err := bot.GetUpdatesChan(ucfg)
+
+	for {
+		select {
+		case update := <-updatesChan:
+			userName := update.Message.From.UserName
+			chatId := update.Message.Chat.ID
+			text := update.Message.Text
+			log.Printf("[%s] %d %s", userName, chatId, text)
+			msg := tgbotapi.NewMessage(chatId, text)
+			bot.Send(msg)
+		}
+	}
+}
