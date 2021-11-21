cfcde477605874b46e787d3621ddc2f37e5d266b
Kosachev Daniil <kos-dan@mail.ru>
2021-11-21 16:45:40 +0300

    feat: added func newQuestionsSequence

--- questions_sequence.go
+++ questions_sequence.go
@@ -4,3 +4,12 @@ type QuestionsSequence struct {
 	i         int
 	questions []string
 }
+
+func newQuestionsSequence(questions []string) *QuestionsSequence {
+	qs := &QuestionsSequence{
+		i:         0,
+		questions: make([]string, len(questions)),
+	}
+	copy(qs.questions, questions)
+	return qs
+}
