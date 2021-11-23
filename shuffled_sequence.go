dad246fc94b1567b6dd820559b4dc477c55b903d
Kosachev Daniil <kos-dan@mail.ru>
2021-11-23 15:07:43 +0300

    feat: added func newShuffledSequence

--- shuffled_sequence.go
+++ shuffled_sequence.go
@@ -1,6 +1,22 @@
 package main
 
+import "math/rand"
+
 type ShuffledSequence struct {
 	i      int
 	values []int
+}
+
+func newShuffledSequence(size int) ShuffledSequence {
+	values := make([]int, size)
+	for i := range values {
+		values[i] = i
+	}
+	rand.Shuffle(size, func(i, j int) {
+		values[i], values[j] = values[j], values[i]
+	})
+	return ShuffledSequence{
+		i:      0,
+		values: values,
+	}
 }
